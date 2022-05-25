9ff7ae563156eedeb8f6c59d9c06425b652ddcc9
HelLuv <works.person@gmail.com>
2022-05-25 21:47:58 +0300

    almost done with header and banner components, added TMDB API, types and styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,9 +7,26 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({}) => {
-  // TODO: Header
+  const [isScrolled, setIsScrolled] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header className="fle items-center space-x-2 md:space-x-10">
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
@@ -22,7 +39,9 @@ const Header: React.FC<HeaderProps> = ({}) => {
         {/*<BasicMenu/>*/}
 
         <ul className="hidden space-x-4 md:flex">
-          <li className="headerLink cursor-default font-semibold text-white hover:text-white">Home</li>
+          <li className="headerLink cursor-default font-semibold text-white hover:text-white">
+            Home
+          </li>
           <li className="headerLink">TV Shows</li>
           <li className="headerLink">Movies</li>
           <li className="headerLink">New & Popular</li>
@@ -36,7 +55,7 @@ const Header: React.FC<HeaderProps> = ({}) => {
         <Link href="/account">
           <img
             src="https://rb.gy/g1pwyx"
-            alt="account link"
+            alt=""
             className="cursor-pointer rounded"
           />
         </Link>
